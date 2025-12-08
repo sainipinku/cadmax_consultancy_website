670b6ec5f4a0d13161abd59b9c2df69c8c3d7388
sainipinku <ramchandrasaini63@gmail.com>
2025-12-08 11:51:08 +0530

    update

--- src/pages/home/components/image.jsx
+++ src/pages/home/components/image.jsx
@@ -1,88 +1,86 @@
 import React from "react";
-import "./image.css"
+import "./image.css";
 
 const MasonryGallery = () => {
   return (
-    <div className="collage-wrapper">
+    <div className="gallery-wrapper">
 
       {/* ------------------ TOP COLLAGE ------------------ */}
-      <div className="top-collage">
+      <div className="gallery-top">
 
-        
-        <div className="col-3-md coll">
-          <div className="box tall">
+        <div className="gallery-col">
+          <div className="gallery-card tall">
             <img src="https://cadmaxpro-buket.s3.ap-south-1.amazonaws.com/assets/services/LANDMARK-001.jpg" />
-            <div className="overlay" />
-            <div className="title">HIGH-ACCURACY DGPS SURVEY</div>
+            <div className="gallery-overlay" />
+            <div className="gallery-title">HIGH-ACCURACY DGPS SURVEY</div>
           </div>
         </div>
 
-        
-        <div className="col-3-md coll">
-          <div className="box big">
+        <div className="gallery-col">
+          <div className="gallery-card big">
             <img src="https://cadmaxpro-buket.s3.ap-south-1.amazonaws.com/assets/home/addnew.jpg" />
-            <div className="overlay" />
-            <div className="title">ENGINEERING SURVEY EXECUTION</div>
+            <div className="gallery-overlay" />
+            <div className="gallery-title">ENGINEERING SURVEY EXECUTION</div>
           </div>
 
-          <div className="box small">
+          <div className="gallery-card small">
             <img src="https://cadmaxpro-buket.s3.ap-south-1.amazonaws.com/assets/home/addnew.jpg" />
-            <div className="overlay" />
-            <div className="title">TOPOGRAPHIC GROUND MODELLING</div>
+            <div className="gallery-overlay" />
+            <div className="gallery-title">TOPOGRAPHIC GROUND MODELLING</div>
           </div>
         </div>
 
-        
-        <div className="col-3-md coll">
-          <div className="box small">
+        <div className="gallery-col">
+          <div className="gallery-card small">
             <img src="https://cadmaxpro-buket.s3.ap-south-1.amazonaws.com/assets/work/Cadmax.jpg" />
-            <div className="overlay" />
-            <div className="title">LAND BOUNDARY AND CONDITION SURVEYING</div>
+            <div className="gallery-overlay" />
+            <div className="gallery-title">LAND BOUNDARY AND CONDITION SURVEYING</div>
           </div>
 
-          <div className="box big">
+          <div className="gallery-card big">
             <img src="https://cadmaxpro-buket.s3.ap-south-1.amazonaws.com/assets/services/ServicesPhoto.jpg" />
-            <div className="overlay" />
-            <div className="title">MOBILE MAPPING + GIS PRODUCTION</div>
+            <div className="gallery-overlay" />
+            <div className="gallery-title">MOBILE MAPPING + GIS PRODUCTION</div>
           </div>
         </div>
 
-       
-        <div className="col-3-md coll">
-          <div className="box big">
+        <div className="gallery-col">
+          <div className="gallery-card big">
             <img src="https://cadmaxpro-buket.s3.ap-south-1.amazonaws.com/assets/services/ServicesPhoto.jpg" />
-            <div className="overlay" />
-            <div className="title">MEP DESIGN ENGINEERING</div>
+            <div className="gallery-overlay" />
+            <div className="gallery-title">MEP DESIGN ENGINEERING</div>
           </div>
 
-          <div className="box small">
+          <div className="gallery-card small">
             <img src="https://cadmaxpro-buket.s3.ap-south-1.amazonaws.com/assets/services/LANDMARK-001.jpg" />
-            <div className="overlay" />
-            <div className="title">URBAN AND SUBDIVISION PLANNING</div>
+            <div className="gallery-overlay" />
+            <div className="gallery-title">URBAN AND SUBDIVISION PLANNING</div>
           </div>
         </div>
 
       </div>
 
       {/* ------------------ BOTTOM COLLAGE ------------------ */}
-      <div className="bottom-collage">
-        <div className="box bottom-box">
+      <div className="gallery-bottom">
+
+        <div className="gallery-card bottom-card">
           <img src="https://cadmaxpro-buket.s3.ap-south-1.amazonaws.com/assets/services/LANDMARK-001.jpg" />
-          <div className="overlay" />
-          <div className="title">ON-SITE LAYOUT MARKING</div>
+          <div className="gallery-overlay" />
+          <div className="gallery-title">ON-SITE LAYOUT MARKING</div>
         </div>
 
-        <div className="box bottom-box">
+        <div className="gallery-card bottom-card">
           <img src="https://cadmaxpro-buket.s3.ap-south-1.amazonaws.com/assets/services/ServicesPhoto.jpg" />
-          <div className="overlay" />
-          <div className="title">GEODETIC AND ADVANCED POSITIONING</div>
+          <div className="gallery-overlay" />
+          <div className="gallery-title">GEODETIC AND ADVANCED POSITIONING</div>
         </div>
 
-        <div className="box bottom-box">
+        <div className="gallery-card bottom-card">
           <img src="https://cadmaxpro-buket.s3.ap-south-1.amazonaws.com/assets/home/addnew.jpg" />
-          <div className="overlay" />
-          <div className="title">INFRASTRUCTURE SURVEY SYSTEMS</div>
+          <div className="gallery-overlay" />
+          <div className="gallery-title">INFRASTRUCTURE SURVEY SYSTEMS</div>
         </div>
+
       </div>
     </div>
   );
