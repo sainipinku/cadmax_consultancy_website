5c540c460998308fdda9ed0d5cadc43525ba73a4
sainipinku <ramchandrasaini63@gmail.com>
2025-12-17 12:23:45 +0530

    update

--- src/pages/home/components/company.jsx
+++ src/pages/home/components/company.jsx
@@ -14,8 +14,45 @@ import img10 from "../../../assets/Images/company logo/JECRC FOUNDATION LOGO.png
 import img11 from "../../../assets/Images/company logo/Shriram-Group-Logo.png";
 import img12 from "../../../assets/Images/company logo/UDAY GAJRAJ logo.jpg";
 import img13 from "../../../assets/Images/company logo/chiripal-logo.png";
+import img14 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.21 AM.jpeg";
+import img15 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.22 AM.jpeg";
+import img16 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.23 AM (1).jpeg";
+import img17 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.23 AM.jpeg";
+import img18 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.24 AM (1).jpeg";
+import img19 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.24 AM.jpeg";
+import img20 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.25 AM (1).jpeg";
+import img21 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.25 AM.jpeg";
+import img22 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.26 AM.jpeg";
+import img23 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.27 AM (1).jpeg";
+import img24 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.27 AM.jpeg";
+import img25 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.28 AM (1).jpeg";
+import img26 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.28 AM.jpeg";
+import img27 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.29 AM (1).jpeg";
+import img28 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.29 AM.jpeg";
+import img29 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.30 AM (1).jpeg";
+import img30 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.30 AM (2).jpeg";
+import img31 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.30 AM.jpeg";
+import img32 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.31 AM (1).jpeg";
+import img33 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.31 AM (2).jpeg";
+import img34 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.31 AM.jpeg";
+import img35 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.32 AM (1).jpeg";
+import img36 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.32 AM.jpeg";
+import img37 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.33 AM (1).jpeg";
+import img38 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.33 AM (2).jpeg";
+import img39 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.33 AM.jpeg";
+import img40 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.34 AM (1).jpeg";
+import img41 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.34 AM.jpeg";
+import img42 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.35 AM (1).jpeg";
+import img43 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.35 AM.jpeg";
+import img44 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.36 AM (1).jpeg";
+import img45 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.36 AM (2).jpeg";
+import img46 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.36 AM.jpeg";
+import img47 from "../../../assets/Images/company logo/WhatsApp Image 2025-12-17 at 11.35.37 AM.jpeg";
 
-const images = [img5, img6, img7, img8, img9, img10, img11, img12 ,img13];
+
+
+
+const images = [img5, img6, img7, img8, img9, img10, img11, img12 ,img13 ,img14 ,img15 ,img16 ,img17 , img18  ,img19 , img20 , img21 ,img22 , img23 , img24 ,img25 ,img26 , img27 ,img28 , img29 , img30 ,img31 ,img32 ,img33 ,img34 , img35 ,img36 ,img37 , img38 , img39 , img40 , img41 , img42 , img43 , img44 , img45 , img46 , img47     ];
 
 const CompanyShowcase = () => {
   const sectionRef = useRef(null);
@@ -27,9 +64,9 @@ const CompanyShowcase = () => {
     setCenterIndex((prev) => (prev + 1) % images.length);
   };
 
-  const prevSlide = () => {
-    setCenterIndex((prev) => (prev - 1 + images.length) % images.length);
-  };
+  // const prevSlide = () => {
+  //   setCenterIndex((prev) => (prev - 1 + images.length) % images.length);
+  // };
 
   useEffect(() => {
     const observer = new IntersectionObserver(
