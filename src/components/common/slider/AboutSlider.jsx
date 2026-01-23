03dafc9d17d8238afea172f47db386c8e2ca71ce
sainipinku <ramchandrasaini63@gmail.com>
2026-01-23 13:34:49 +0530

    update

--- src/components/common/slider/AboutSlider.jsx
+++ src/components/common/slider/AboutSlider.jsx
@@ -63,7 +63,7 @@ const AboutSlider = () => {
       <motion.p
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.9, ease: "easeOut", delay: 0.2 }}
+        transition={{ duration: 0.5, ease: "easeOut", delay: 0.1 }}
         viewport={{ once: true }}
         className="text-center max-w-4xl mx-auto text-base md:text-lg text-gray-600 mb-14 leading-relaxed"
       >
