4412291cc22eccdc49744b578b25789ab8722422
sainipinku <ramchandrasaini63@gmail.com>
2026-02-19 14:09:04 +0530

    update

--- src/pages/Services/interior-sub-page/DipendraSite.jsx
+++ src/pages/Services/interior-sub-page/DipendraSite.jsx
@@ -11,45 +11,49 @@ import img5 from "../../../assets/Images/interior-image/HOUSE-DRAWINGROOM-VIEW.p
 import img6 from "../../../assets/Images/interior-image/Drawing-Room-View-2.png";
 
 const DipendraSite = () => {
-    const images = [img1, img2, img3, img4, img5,img6];
-  return (
-      <>
-    <Navbar/>
-    
-    
-    <section className="py-20 px-4 md:px-10 lg:px-20 bg-gray-50">
-        <h2 className="text-3xl pt-4 md:text-4xl font-bold text-center mb-12 tracking-wide">
-          DIPENDRA JI GONER SITE
-        </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          {images.map((image, index) => (
-            <div
-              key={index}
-              className="overflow-hidden rounded-xl border border-gray-200 shadow-md"
-            >
-              <img
-                src={image}
-                alt={`interior-${index}`}
-                className="w-full h-[300px] md:h-[350px] object-cover transition duration-500 hover:scale-110"
-              />
-            </div>
+    const images = [img1, img2, img3, img4, img5, img6];
+
+    return (
+        <>
 
 
-
-          ))}
-
+            <Navbar />
 
 
-        </div>
+            <section className="py-20 px-4 md:px-10 lg:px-20 bg-gray-50">
+                <h2 className="text-3xl pt-4 md:text-4xl font-bold text-center mb-12 tracking-wide">
+                    DIPENDRA JI GONER SITE
+                </h2>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                    {images.map((image, index) => (
+                        <div
+                            key={index}
+                            className="overflow-hidden rounded-xl border border-gray-200 shadow-md"
+                        >
+                            <img
+                                src={image}
+                                alt={`interior-${index}`}
+                                className="w-full h-[300px] md:h-[350px] object-cover transition duration-500 hover:scale-110"
+                            />
+                        </div>
 
 
 
-      </section>
-    
-    <Footer/>
-    </>
-  )
+                    ))}
+
+
+
+                </div>
+
+
+
+            </section>
+
+            <Footer />
+        </>
+    )
 }
 
 export default DipendraSite
